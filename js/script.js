7faf00b4ca9da7553bc1838721d4db5d88151fc0
Urszula Puczynska <puczynska.u@gmail.com>
2023-08-31 19:16:52 +0200

    layout changes

--- js/script.js
+++ js/script.js
@@ -1,24 +1,31 @@
 //navigation open search bar
-const searchSection = document.querySelector(".search__section-navigation");
-const btnSearchBar = document.querySelector(".loop");
+const navbarEl = document.querySelector(".navbar");
+const btnSearchBar = document.querySelector(".loop-nav");
 
-// btnSearchBar.onclick = function () {
-//   searchSection.classList.toggle("nav-open");
-// };
+btnSearchBar.onclick = function () {
+  navbarEl.classList.toggle("nav-open");
+};
 
-//navigation appear aside
-
+//navigation appear aside enable scrolling the page after clicking on 'eksploruj' button
+const exploreBtn = document.querySelector(".header__container-button");
 const headerSection = document.querySelector(".header");
 
 const obs = new IntersectionObserver(
   function (entries) {
     const ent = entries[0];
+    if (ent.isIntersecting === true) {
+      exploreBtn.onclick = function () {
+        document.body.classList.remove("overflow");
+      };
+    }
 
     if (ent.isIntersecting === false) {
-      document.body.classList.add("sticky");
+      document.body.classList.add("open");
+      document.body.classList.remove("overflow");
     }
     if (ent.isIntersecting === true) {
-      document.body.classList.remove("sticky");
+      document.body.classList.remove("open");
+      navbarEl.classList.remove("nav-open");
     }
   },
   {
@@ -30,12 +37,34 @@ const obs = new IntersectionObserver(
 obs.observe(headerSection);
 
 //close open nav by click on a body
-const inputEl = document.querySelector(".input");
-const btnEl = document.getElementById("btn");
+const inputEl = document.querySelector(".nav-input-search");
+const btnEl = document.getElementById("button");
 
-document.onclick = function (e) {
+document.body.onclick = function (e) {
   if (!inputEl.contains(e.target) && !btnEl.contains(e.target)) {
-    document.classList.remove("nav-open");
+    navbarEl.classList.remove("nav-open");
   }
-  searchSection.classList.toggle("nav-open");
 };
+
+//clearing the input after enter button is clicked
+const enterBtnEl = document.querySelector(".enter-icon-input");
+
+enterBtnEl.onclick = function () {
+  inputEl.value = "";
+};
+
+// document.body.addEventListener("keyup", (key) => {
+//   if (key.value === "Enter") {
+//     enterBtnEl.add.click;
+//   }
+// });
+
+enterBtnEl.addEventListener("click", (ev) => {});
+
+document.addEventListener("keypress", (event) => {
+  const keyValue = event.keyValue ? event.keyValue : event.which;
+
+  if (keyValue === 13) {
+    enterBtnEl.click();
+  }
+});
